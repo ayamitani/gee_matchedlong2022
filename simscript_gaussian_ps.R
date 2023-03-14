9ba7e13c5a775c016a8fed0173f62b809ab81975
PeiyuBellaLi <lipeiyu0621@gmail.com>
2023-03-14 11:27:03 -0400

    modified

--- simscript_gaussian_ps.R
+++ simscript_gaussian_ps.R
@@ -1,19 +1,47 @@
 ### generate big cohort 
 
 # total sample size
-N <- 500
+N <- 250
 # total number of visits
 maxT <- 5
 # vector of visits for all patients
-visit <- rep(1:maxT, 500)
+visit <- rep(1:maxT, N)
 # vector of patient ids
 id <- rep(1:N, each = maxT)
 
+simnum <- 5
+estbeta_ind <- vector("list", length = simnum)
+estbeta_exch <- vector("list", length = simnum)
+estbeta_ar1 <- vector("list", length = simnum)
+estbeta_unstr <- vector("list", length = simnum)
+se_ind <- vector("list", length = simnum)
+se_exch <- vector("list", length = simnum)
+se_ar1 <- vector("list", length = simnum)
+se_unstr <- vector("list", length = simnum)
+
+
+estbeta_inds <- vector("list", length = simnum)
+estbeta_exchs <- vector("list", length = simnum)
+estbeta_ar1s <- vector("list", length = simnum)
+estbeta_unstrs <- vector("list", length = simnum)
+se_inds <- vector("list", length = simnum)
+se_exchs <- vector("list", length = simnum)
+se_ar1s <- vector("list", length = simnum)
+se_unstrs <- vector("list", length = simnum)
+sample_size <- vector("list", length = simnum)
+
+
 # baseline covariates
+b1_r <- 30
+b2_r <- 0.5
+b3_r <- -0.2
+b4_r <- 0.5
+
 b1 <- 4
 b2 <- -0.1
 b3 <- -1.3
 b4 <- 1.5
+for (s in 1:simnum) {
 agei <- rnorm(N, mean = 60, sd = 10)
 femalei <- rbinom(N, size = 1, prob = 0.3)
 bsa_bli <- rnorm(N, mean = 2, sd = 0.2)
@@ -28,10 +56,7 @@ bsa_bl <- rep(bsa_bli, each = maxT)
 bav <- rep(bavi, each = maxT)
 
 # generate outcome values
-b1_r <- 30
-b2_r <- 0.5
-b3_r <- -0.2
-b4_r <- 0.5
+
 bi <- rnorm(N, mean = 0, sd = 5)
 b <- rep(bi, each = maxT)
 e <- rnorm(N*maxT, mean = 0, sd = 1)
@@ -62,17 +87,7 @@ matched_long <- simdat[simdat$id %in% matched_base$id,] # sample data
 # repeat 1000 times and get mean coefs and ses
 
 # 1) entire cohort
-simnum <- 1000
-estbeta_ind <- vector("list", length = simnum)
-estbeta_exch <- vector("list", length = simnum)
-estbeta_ar1 <- vector("list", length = simnum)
-estbeta_unstr <- vector("list", length = simnum)
-se_ind <- vector("list", length = simnum)
-se_exch <- vector("list", length = simnum)
-se_ar1 <- vector("list", length = simnum)
-se_unstr <- vector("list", length = simnum)
 
-for (s in 1:simnum) {
   gee_ind <- geeglm(root ~ visit + bav:visit, data = simdat, family = gaussian,
                     id = id, waves = visit, corstr = "independence")
   estbeta_ind[[s]] <- coef(gee_ind) # parameter estimates
@@ -92,6 +107,31 @@ for (s in 1:simnum) {
                     id = id, waves = visit, corstr = "unstructured")
   estbeta_unstr[[s]] <- coef(gee_unstr)
   se_unstr[[s]] <- summary(gee_unstr)$coefficient[,2] 
+  
+  
+  gee_inds <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
+                     id = id, waves = visit, corstr = "independence")
+  estbeta_inds[[s]] <- coef(gee_inds) # parameter estimates
+  se_inds[[s]] <- summary(gee_inds)$coefficient[,2] # standard errors
+  
+  gee_exchs <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
+                      id = id, waves = visit, corstr = "exchangeable")
+  estbeta_exchs[[s]] <- coef(gee_exchs)
+  se_exchs[[s]] <- summary(gee_exchs)$coefficient[,2]
+  
+  gee_ar1s <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
+                     id = id, waves = visit, corstr = "ar1")
+  estbeta_ar1s[[s]] <- coef(gee_ar1s)
+  se_ar1s[[s]] <- summary(gee_ar1s)$coefficient[,2]  
+  
+  gee_unstrs <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
+                       id = id, waves = visit, corstr = "unstructured")
+  estbeta_unstrs[[s]] <- coef(gee_unstrs)
+  se_unstrs[[s]] <- summary(gee_unstrs)$coefficient[,2] 
+  
+  sample_size[[s]] <- nrow(matched_base)
+  
+  print(s)
 }
 
 beta_mean_ind <- colMeans(do.call("rbind", estbeta_ind)) 
@@ -112,35 +152,10 @@ kbl(out_cohort,digit = 2, escape = FALSE, caption = "GEE estimates and SEs for t
 
 
 # 2) sample data
-estbeta_inds <- vector("list", length = simnum)
-estbeta_exchs <- vector("list", length = simnum)
-estbeta_ar1s <- vector("list", length = simnum)
-estbeta_unstrs <- vector("list", length = simnum)
-se_inds <- vector("list", length = simnum)
-se_exchs <- vector("list", length = simnum)
-se_ar1s <- vector("list", length = simnum)
-se_unstrs <- vector("list", length = simnum)
+
 
 for (s in 1:simnum) {
-  gee_inds <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
-                    id = id, waves = visit, corstr = "independence")
-  estbeta_inds[[s]] <- coef(gee_inds) # parameter estimates
-  se_inds[[s]] <- summary(gee_inds)$coefficient[,2] # standard errors
-  
-  gee_exchs <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
-                     id = id, waves = visit, corstr = "exchangeable")
-  estbeta_exchs[[s]] <- coef(gee_exchs)
-  se_exchs[[s]] <- summary(gee_exchs)$coefficient[,2]
-  
-  gee_ar1s <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
-                    id = id, waves = visit, corstr = "ar1")
-  estbeta_ar1s[[s]] <- coef(gee_ar1s)
-  se_ar1s[[s]] <- summary(gee_ar1s)$coefficient[,2]  
-  
-  gee_unstrs <- geeglm(root ~  visit + bav:visit, data = matched_long, family = gaussian,
-                      id = id, waves = visit, corstr = "unstructured")
-  estbeta_unstrs[[s]] <- coef(gee_unstrs)
-  se_unstrs[[s]] <- summary(gee_unstrs)$coefficient[,2] 
+
 }
 
 beta_mean_inds <- colMeans(do.call("rbind", estbeta_inds)) 
